b9b44a53fc1c08f567d48504413a1974d6cb78b2
Adrian W <windisch.adrian09@gmail.com>
2021-10-16 01:11:49 +0300

    a

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ const altName = ['Mondei', 'Tvesdei', 'Vednesdei', 'Sărzdei', 'Freidei', 'Setur
 const date = new Date();
 
 function change() {
-    var channel =  client.channels.cache.get('698958944122699878');
+    var channel =  client.channels.cache.get('698958944122699878a');
     var day = date.getDay();
     var newName = 'Welfare ' + altName[day-1];
 
     channel.setName(newName);
+    console.log(client.channels.cache);
 }
 
 
