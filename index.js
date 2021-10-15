4266ac062ec22925e59daaf1d80316c75c44a437
Adrian W <windisch.adrian09@gmail.com>
2021-10-15 21:54:49 +0300

    fix

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ client.on("ready", () => {
 })
 
 client.on('message', message => {
-    if (message.author.bot) {
-        return
-    }
+    if (message.author.bot) return;
     if (message.content.match('m*i*a*u*') {
         message.reply('Imi bag pula in mieunatu tau coaie');
     }
