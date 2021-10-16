f6d059ad595a589f487b2554c2d5351e89296727
Adrian W <windisch.adrian09@gmail.com>
2021-10-16 12:21:11 +0300

    'node-cron isntead of cron'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const cron = require("cron");
+const cron = require("node-cron");
 process.env.TZ = 'Europe/Athens';
 client.login(process.env.BOT_TOKEN);
 
