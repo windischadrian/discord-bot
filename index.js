0f6d569f04870bf8de59fd36232160960a853f23
Adrian W <windisch.adrian09@gmail.com>
2021-10-16 17:19:40 +0300

    'vulpea la arici fix'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const CronJob = require('cron').CronJob;
-const { bahoiSuicid } = require('./bahoi.js');
+import { bahoiSuicid } from './bahoi';
 client.login(process.env.BOT_TOKEN);
 
 client.on("ready", () => {
