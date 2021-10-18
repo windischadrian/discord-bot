a5122563a22b252630f5bb5e72954b6af43c96b7
Adrian W <windisch.adrian09@gmail.com>
2021-10-18 14:59:39 +0300

    'change prefix to / dont fuck up pls'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_S
 const CronJob = require('cron').CronJob;
 const bahoiSuicid = 'Oricâte probleme ai avea, nu-i bine să te omori ca țăranu...te omori ieee ca pizdele alea \'mă omor miaaa\', dar tu nu mai exiști, adică tu crezi că ajungi in altă lume și pula mea cu spiritul tău..NU! Spiritul rămane...in pizda mă-tii1 Știți ce e important? Să futi, să te distrezi, să bei, mici, bere, ăăăă și toate cele, pepeni, struguri, ciorchini, toate distracțiile toate panaramele sunt atâtea carusele sunt atâtea mașinării care să apeși pe butoane sunt atâtea magarii sa canti sa vioara sa toate alea ce dracu ma te omori ce pizda mă-...psaico maico ca esti bolnav la cap că nu te placi ca pula mea ca pula-n pizdă da mai lasă-mă-n pula mea crezi ca mă omor acuma ca dacă nu mai nu stiu ce mai lasă nu știu ce...\'mă omor\' vai băga-mi-aș pula-n mă-ta te omori? Mai lasă-mă in pula mea cu omorâtu pizdii.';
 const playdl = require('play-dl');
-const prefix = '?';
+const prefix = '/';
 isready = false;
 
 const queue = new Map();
@@ -149,9 +149,9 @@ async function executePlay(audioName, message, voiceChannel) {
     try {
         var songInfo;
         if (!audioName.includes('www.youtube.com/watch?v=')) {
-            message.suppressEmbeds(true);
             songInfo = await searchYoutubeAsync(audioName);
         } else {
+            message.suppressEmbeds(true);
             audioName=audioName.trim();
             songInfo = await searchYoutubeByUrlAsync(audioName);
         }
