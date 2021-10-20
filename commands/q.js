5ce18eba21f3c1a9eb36f3c1bbcfe338a681067d
Adrian W <windisch.adrian09@gmail.com>
2021-10-20 15:05:58 +0300

    'queue embed msg attempt2'

--- commands/q.js
+++ commands/q.js
@@ -12,20 +12,25 @@ exports.run = (client, message) => {
     var totalDuration = 0;
     var i = 1;
 
-    const messageEmbed = new MessageEmbed()
-        .setTitle('**Songs in queueueueueueue:**')
-        .addField('Currently playing', serverQueue.songPlayingTitle, false);
+    var messageEmbed = new MessageEmbed()
+        .setTitle('**Songs in queueueueueueue:**');
+    var currentlyPlaying = serverQueue.songPlayingTitle;
+    if(!currentlyPlaying) currentlyPlaying = 'No song currently playing';
+    messageEmbed.addField('Currently playing', currentlyPlaying, false);
+    console.log(currentlyPlaying);
 
     serverQueue.songs.forEach(song => {
         qMessage+= `**${i} - ${song.title} - ${song.duration}**\n`;
         totalDuration+=song.durationSeconds;
         i++;
     });
+    console.log(qMessage);
 
     messageEmbed.addField('Music queueueueueueue', qMessage, false);
     var date = new Date(null);
     date.setSeconds(totalDuration);
     // qMessage+=`\n**Total queueueueueueue duration: ${date.toISOString().substr(11,8)}**`
+    console.log(date);
     
     messageEmbed.addField('Total duration: ',  date.toISOString().substr(11,8), false);
     message.channel.send(messageEmbed);
