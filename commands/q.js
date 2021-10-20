9e92b96dadabadbae7777ab437ca5bf459acb5ef
Adrian W <windisch.adrian09@gmail.com>
2021-10-20 14:29:29 +0300

    'log'

--- commands/q.js
+++ commands/q.js
@@ -1,3 +1,6 @@
+const { MessageEmbed } = require("discord.js");
+const { songsPerpage } = require("../config.json");
+
 exports.run = (client, message) => {
     const queue = client.queue;
     const serverQueue = queue.get(message.guild.id);
@@ -7,11 +10,18 @@ exports.run = (client, message) => {
     var qMessage = '**Songs in queueueueueueue:**\n';
     var totalDuration = 0;
     var i = 1;
+
+    const messageEmbed = new MessageEmbed()
+        .setTitle('**Songs in queueueueueueue:**')
+
     serverQueue.songs.forEach(song => {
+        messageEmbed.addField(null, )
         qMessage+= `**${i} - ${song.title} - ${song.duration}**\n`;
         totalDuration+=song.durationSeconds;
         i++;
     });
+
+    console.log('songs per page: ' + songsPerpage);
     var date = new Date(null);
     date.setSeconds(totalDuration);
     qMessage+=`\n**Total queueueueueueue duration: ${date.toISOString().substr(11,8)}**`
