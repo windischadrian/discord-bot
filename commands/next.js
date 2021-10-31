5f2c35a89124e9e7d59062d68f68f67f8380d92e
Adrian W <windisch.adrian09@gmail.com>
2021-10-31 21:42:09 +0200

    fix next.js

--- commands/next.js
+++ commands/next.js
@@ -2,15 +2,17 @@ const playCommand = require('./play.js');
 
 exports.run = (client, message, args) => {
     console.log('args: ' + args);
+    console.log('isArray: ' + message);
+    console.log('message: ' + Array.isArray(args));
 
     const guildId = message.guild.id;
     const serverQueue = client.queue.get(guildId);
 
     if (!serverQueue) return message.reply('Not playing any songs or some shit.');
 
-    if (args.length() > 1 ) return message.reply('Too many arguments.');
+    if (Array.isArray(args)) return message.reply('Too many arguments.');
 
-    const position = args[0];
+    const position = args;
     if (! typeof position == 'number') return message.reply('Argument not a number.');
 
     serverQueue.songs.unshift(serverQueue.songs.splice(pos, 1)[0])
