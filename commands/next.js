0cdc8916795f2e8c95eceabc611933d26d03231f
Adrian W <windisch.adrian09@gmail.com>
2021-10-31 21:49:59 +0200

    fix stupidity in next js

--- commands/next.js
+++ commands/next.js
@@ -1,18 +1,15 @@
 const playCommand = require('./play.js');
 
 exports.run = (client, message, args) => {
-    console.log('args: ' + args);
-    console.log('isArray: ' + message);
-    console.log('message: ' + Array.isArray(args));
 
     const guildId = message.guild.id;
     const serverQueue = client.queue.get(guildId);
 
     if (!serverQueue) return message.reply('Not playing any songs or some shit.');
 
-    if (Array.isArray(args)) return message.reply('Too many arguments.');
+    if (args.length > 1) return message.reply('Too many arguments.');
 
-    const position = args;
+    const position = args[0];
     if (! typeof position == 'number') return message.reply('Argument not a number.');
 
     serverQueue.songs.unshift(serverQueue.songs.splice(pos, 1)[0])
