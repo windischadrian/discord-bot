a7a7e79d748e3886ffde7124e5ef2b42f717549e
Adrian W <windisch.adrian09@gmail.com>
2021-10-31 21:17:26 +0200

    added next command

---
+++ commands/next.js
@@ -0,0 +1,19 @@
+const playCommand = require('./play.js');
+
+exports.run = (client, message, args) => {
+    console.log('args: ' + args);
+
+    const guildId = message.guild.id;
+    const serverQueue = client.queue.get(guildId);
+
+    if (!serverQueue) return message.reply('Not playing any songs or some shit.');
+
+    if (args.length() > 1 ) return message.reply('Too many arguments.');
+
+    const position = args[0];
+    if (! typeof position == 'number') return message.reply('Argument not a number.');
+
+    serverQueue.songs.unshift(serverQueue.songs.splice(pos, 1)[0])
+    message.react('✅')
+
+}
