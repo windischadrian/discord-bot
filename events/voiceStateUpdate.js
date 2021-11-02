1b034c8c58b72ed4348400a6515a2728f2f80be1
Adrian W <windisch.adrian09@gmail.com>
2021-11-02 14:33:01 +0200

    fix stupidity, voice channel not text channel

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -7,7 +7,6 @@ module.exports = async (client, oldState, newState) => {
     if (!oldState.channel.members.size - 1) 
     setTimeout(() => { // if 1 (you), wait five minutes
         if (!oldState.channel.members.size - 1) // if there's still 1 member, 
-            oldState.channel.send('Y\'all left me here by myself, imma leave too.');
             oldState.channel.leave(); // leave
     }, 100000); // (5 min in ms)
 
