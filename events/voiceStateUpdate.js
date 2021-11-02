312a5779242ca3229e684eab6099d916c3c61636
Adrian W <windisch.adrian09@gmail.com>
2021-11-02 16:16:55 +0200

    im retarded

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,19 +1,15 @@
 module.exports = async (client, oldState, newState) => {
 
     // if nobody left the channel in question, return.
-    console.log(oldState.channelId);
-    console.log(oldState.guild.me.voice.channelId);
-    console.log(newState);
     if (oldState.channelId !==  oldState.guild.me.voice.channelId || newState.channel) return;
 
     // otherwise, check how many people are in the channel now
-    console.log(oldState.channel.members.size);
     if (!oldState.channel.members.size - 1) 
     setTimeout(() => { // if 1 (you), wait five minutes
         if (!oldState.channel.members.size - 1) // if there's still 1 member, 
             {
-                oldState.guild.me.voice.voice.channel.leave(); // leave
-                console.log('leave');
+                oldState.guild.me.voice.channel.leave(); // leave
+                console.log('Inactive voice channel, leaving.');
             }
     }, 100000); // (5 min in ms)
 
