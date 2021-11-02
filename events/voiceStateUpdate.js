3b96ead13680ffed2e5a15b61bc8335ac210fbc9
Adrian W <windisch.adrian09@gmail.com>
2021-11-02 14:19:44 +0200

    leave if voice channel empty

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,16 +1,13 @@
 module.exports = async (client, oldState, newState) => {
-    // const date = new Date();
-    // const allowedUsers = ['356698378652090380', '95590636408803328', '406955988546224128', '237939756959268865', '215499279756820480']
-    // if(newState.channelId === "698958944122699878" && allowedUsers.includes(newState.member.id))
-    // { 
-    //     await newState.member.createDM();
-    //     await newState.member.send(`Ia te uita, e ora ${date.toLocaleTimeString()}, ora perfecta sa sugi pula.`)
-    // }
 
-    // if(oldState.channelId === "698958944122699878" && allowedUsers.includes(newState.member.id))
-    // { 
-    //     await oldState.member.createDM();
-    //     await oldState.member.send(`Ai plecat sa sugi pula, este?`);
-    // }
+    // if nobody left the channel in question, return.
+    if (oldState.channelID !==  oldState.guild.me.voice.channelID || newState.channel) return;
+
+    // otherwise, check how many people are in the channel now
+    if (!oldState.channel.members.size - 1) 
+    setTimeout(() => { // if 1 (you), wait five minutes
+        if (!oldState.channel.members.size - 1) // if there's still 1 member, 
+            oldState.channel.leave(); // leave
+    }, 300000); // (5 min in ms)
 
 };
